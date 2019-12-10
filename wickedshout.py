cd189f0815fe0e8d803c64a1bb71b4bf62e01d65
Frank Lazzarini <flazzarini@gmail.com>
2019-12-11 00:03:23 +0100

    Added more structure to shouts

--- wickedshout.py
+++ wickedshout.py
@@ -1,9 +1,74 @@
-# minqlx plugin
-# Copyright (C) 2015 Mino <mino@minomino.org>
+from collections import namedtuple
+from datetime import datetime
+from os.path import join
+from random import choice
+from typing import Optional, List
 
 import minqlx
-
 from minqlx.database import Redis
+
+
+class Shout(namedtuple('Shout', ['pack', 'alias', 'simplified_path'])):
+    PREFIX_PATH = 'sound/wicked'
+
+    @property
+    def path(self) -> str:
+        if not self.simplified_path.startswith('sound/wicked'):
+            return join(self.PREFIX_PATH, self.simplified_path)
+        return self.simplified_path
+
+
+class Shoutdb:
+    DB = {}
+
+    def register_shouts(self, shouts: List[Shout]) -> None:
+        """
+        Loops and delegates to self.register
+        """
+        for shout in shouts:
+            self.register(shout)
+
+    def register(self, shout: Shout) -> None:
+        """
+        Registers a new Shout to the "database"
+        """
+        if shout.pack not in self.DB:
+            self.DB[shout.pack] = {}
+
+        self.DB[shout.pack.lower()][shout.alias.lower()] = shout
+
+    def get_shout(self, shout_pack: str, shout_alias: str) -> Optional[Shout]:
+        """
+        Retrieve matching shout from the "database"
+        """
+        return self.DB.get(shout_pack, {}).get(shout_alias)
+
+    def get_random_shout(self) -> Optional[Shout]:
+        """
+        Get a random shout, by default return a standard sound
+        """
+        if not self.DB:
+            return Shout('default', 'quakelive',
+                         'sound/player/razor/taunt.wav')
+
+        pack = choice(self.get_shout_packs())
+        chosen_shout = choice(self.get_shouts_in_pack(pack))
+        return chosen_shout
+
+    def get_shout_packs(self) -> List[str]:
+        """
+        Get a list of available shout packs
+        """
+        return list(self.DB.keys())
+
+    def get_shouts_in_pack(self, shout_pack: str) -> List[Shout]:
+        """
+        Returns a list of shouts for a given shout_pack
+        """
+        if shout_pack not in self.DB:
+            return []
+
+        return list(self.DB[shout_pack].values())
 
 
 class wickedshout(minqlx.Plugin):
@@ -14,10 +79,12 @@ class wickedshout(minqlx.Plugin):
 
     def __init__(self):
         super().__init__()
-        self._mapping = None  # Cached variable (set in self.mapping property)
+        self.shoutdb = Shoutdb()
+        self.register_shouts()
 
         self.add_command("shout", self.shout)
         self.add_command("shoutlist", self.shoutlist)
+        self.add_command("shoutrandom", self.shoutrandom)
 
     def shout(self, player, msg, channel):
         """
@@ -26,18 +93,24 @@ class wickedshout(minqlx.Plugin):
         shout_pack = msg[1]
         shout_sound = msg[2]
 
-        sound_path = self.mapping.get(shout_pack, {}).get(shout_sound)
+        shout = self.shoutdb.get(shout_pack, shout_sound)
 
-        if not sound_path:
+        if not shout:
             channel.reply(
                 "Shout sound ^2%s^7 for shout_pack ^2%s^7 was not found, "
                 "please use ^2!shoutlist^7 to list all available shouts" % (
                     shout_sound, shout_pack))
             return
 
-        channel.reply("Player ^3%s^7 shouts ^2%s %s^7" % (
-            player, shout_pack, shout_sound))
-        self.play_sound(sound_path)
+        self.play_sound(channel, player, shout)
+        return
+
+    def shoutrandom(self, player, msg, channel):
+        """
+        Shouts a random shout
+        """
+        random_shout = self.shoutdb.get_random_shout()
+        self.play_sound(channel, player, random_shout)
         return
 
     def shoutlist(self, player, msg, channel):
@@ -45,179 +118,94 @@ class wickedshout(minqlx.Plugin):
         Print Shoutlist to player
         """
         output = []
-        for shout_pack in sorted(self.mapping.keys()):
+        for shout_pack in sorted(self.shoutdb.get_shout_packs()):
             output.append("^5%s^7\n" % shout_pack)
             output.append("^5%s^7\n" % ("=" * len(shout_pack)))
 
-            shout_sounds = []
-            for shout_sound in sorted(self.mapping.get(shout_pack).keys()):
-                shout_sounds.append("^6%s^7 " % shout_sound)
+            shout_aliases = []
+            for shout in sorted(self.shoutdb.get_shouts_in_pack(shout_pack)):
+                shout_aliases.append("^6%s^7 " % shout.alias)
 
-                if len("".join(shout_sounds)) >= 120:
-                    shout_sounds.append("\n")
-            output += shout_sounds
+                if len("".join(shout_aliases)) >= 120:
+                    shout_aliases.append("\n")
+            output += shout_aliases
             output.append("\n\n")
 
         output.append(
             "\n\nExample Usage: ^3\\say !shout wickedq1-badblood 02^7")
+        output.append(
+            "\n\nShout a random sound: ^3\\say !shoutrandom^7")
 
         player.tell("".join(output))
 
-    def play_sound(self, path):
+    def play_sound(self, channel, player, shout):
         """
         Plays sound stored in `path`
         """
+        if not self.player_is_allowed(player):
+            player.tell("^1Stop shouting for a few seconds^7")
+            return
+
+        channel.reply("Player ^3%s^7 shouts ^2%s %s^7" % (
+            player, shout.pack, shout.alias))
+
         for p in self.players():
-            super().play_sound(path, p)
+            super().play_sound(shout.path, p)
 
-    @property
-    def mapping(self):
-        if self._mapping:
-            return self._mapping
+    def player_is_allowed(self, player):
+        """
+        Verifies if player has shouted in the last 2 seconds
+        """
+        player_key = "wickedshout_last_%s" % player.steam_id
+        if player_key in self.db:
+            last_played = float(self.db[player_key])
+            now = datetime.now().timestamp()
+            if (now - last_played) < 2.0:
+                return False
 
-        self._mapping = {
-            'wickedq1-badblood': {
-                '01': 'sound/wicked/characters/BadBlood/01.ogg',
-                '02': 'sound/wicked/characters/BadBlood/02.ogg',
-                '03': 'sound/wicked/characters/BadBlood/03.ogg',
-                '04': 'sound/wicked/characters/BadBlood/04.ogg',
-                '05': 'sound/wicked/characters/BadBlood/05.ogg',
-                '06': 'sound/wicked/characters/BadBlood/06.ogg',
-                '07': 'sound/wicked/characters/BadBlood/07.ogg',
-                '08': 'sound/wicked/characters/BadBlood/08.ogg',
-                '09': 'sound/wicked/characters/BadBlood/09.ogg',
-                '10': 'sound/wicked/characters/BadBlood/10.ogg',
-                '11': 'sound/wicked/characters/BadBlood/11.ogg',
-                '12': 'sound/wicked/characters/BadBlood/12.ogg',
-            },
-            'wickedq1-buzz': {
-                '01': 'sound/wicked/characters/Buzz/01.ogg',
-                '02': 'sound/wicked/characters/Buzz/02.ogg',
-                '03': 'sound/wicked/characters/Buzz/03.ogg',
-                '04': 'sound/wicked/characters/Buzz/04.ogg',
-                '05': 'sound/wicked/characters/Buzz/05.ogg',
-                '06': 'sound/wicked/characters/Buzz/06.ogg',
-                '07': 'sound/wicked/characters/Buzz/07.ogg',
-                '08': 'sound/wicked/characters/Buzz/08.ogg',
-                '09': 'sound/wicked/characters/Buzz/09.ogg',
-                '10': 'sound/wicked/characters/Buzz/10.ogg',
-                '11': 'sound/wicked/characters/Buzz/11.ogg',
-                '12': 'sound/wicked/characters/Buzz/12.ogg',
-            },
-            'wickedq1-darthvader': {
-                '01': 'sound/wicked/characters/DarthVader/01.ogg',
-                '02': 'sound/wicked/characters/DarthVader/02.ogg',
-                '03': 'sound/wicked/characters/DarthVader/03.ogg',
-                '04': 'sound/wicked/characters/DarthVader/04.ogg',
-                '05': 'sound/wicked/characters/DarthVader/05.ogg',
-                '06': 'sound/wicked/characters/DarthVader/06.ogg',
-                '07': 'sound/wicked/characters/DarthVader/07.ogg',
-                '08': 'sound/wicked/characters/DarthVader/08.ogg',
-                '09': 'sound/wicked/characters/DarthVader/09.ogg',
-                '10': 'sound/wicked/characters/DarthVader/10.ogg',
-                '11': 'sound/wicked/characters/DarthVader/11.ogg',
-                '12': 'sound/wicked/characters/DarthVader/12.ogg',
-            },
-            'wickedq1-docholiday': {
-                '01': 'sound/wicked/characters/DocHoliday/01.ogg',
-                '02': 'sound/wicked/characters/DocHoliday/02.ogg',
-                '03': 'sound/wicked/characters/DocHoliday/03.ogg',
-                '04': 'sound/wicked/characters/DocHoliday/04.ogg',
-                '05': 'sound/wicked/characters/DocHoliday/05.ogg',
-                '06': 'sound/wicked/characters/DocHoliday/06.ogg',
-                '07': 'sound/wicked/characters/DocHoliday/07.ogg',
-                '08': 'sound/wicked/characters/DocHoliday/08.ogg',
-                '09': 'sound/wicked/characters/DocHoliday/09.ogg',
-                '10': 'sound/wicked/characters/DocHoliday/10.ogg',
-                '11': 'sound/wicked/characters/DocHoliday/11.ogg',
-                '12': 'sound/wicked/characters/DocHoliday/12.ogg',
-            },
-            'wickedq1-moses': {
-                '01': 'sound/wicked/characters/Moses/01.ogg',
-                '02': 'sound/wicked/characters/Moses/02.ogg',
-                '03': 'sound/wicked/characters/Moses/03.ogg',
-                '04': 'sound/wicked/characters/Moses/04.ogg',
-                '05': 'sound/wicked/characters/Moses/05.ogg',
-                '06': 'sound/wicked/characters/Moses/06.ogg',
-                '07': 'sound/wicked/characters/Moses/07.ogg',
-                '08': 'sound/wicked/characters/Moses/08.ogg',
-                '09': 'sound/wicked/characters/Moses/09.ogg',
-                '10': 'sound/wicked/characters/Moses/10.ogg',
-                '11': 'sound/wicked/characters/Moses/11.ogg',
-                '12': 'sound/wicked/characters/Moses/12.ogg',
-            },
-            'wickedq1-peedee': {
-                '01': 'sound/wicked/characters/Peedee/01.ogg',
-                '02': 'sound/wicked/characters/Peedee/02.ogg',
-                '03': 'sound/wicked/characters/Peedee/03.ogg',
-                '04': 'sound/wicked/characters/Peedee/04.ogg',
-                '05': 'sound/wicked/characters/Peedee/05.ogg',
-                '06': 'sound/wicked/characters/Peedee/06.ogg',
-                '07': 'sound/wicked/characters/Peedee/07.ogg',
-                '08': 'sound/wicked/characters/Peedee/08.ogg',
-                '09': 'sound/wicked/characters/Peedee/09.ogg',
-                '10': 'sound/wicked/characters/Peedee/10.ogg',
-                '11': 'sound/wicked/characters/Peedee/11.ogg',
-                '12': 'sound/wicked/characters/Peedee/12.ogg',
-            },
-            'wickedq1-scavenger': {
-                '01': 'sound/wicked/characters/Scavenger/01.ogg',
-                '02': 'sound/wicked/characters/Scavenger/02.ogg',
-                '03': 'sound/wicked/characters/Scavenger/03.ogg',
-                '04': 'sound/wicked/characters/Scavenger/04.ogg',
-                '05': 'sound/wicked/characters/Scavenger/05.ogg',
-                '06': 'sound/wicked/characters/Scavenger/06.ogg',
-                '07': 'sound/wicked/characters/Scavenger/07.ogg',
-                '08': 'sound/wicked/characters/Scavenger/08.ogg',
-                '09': 'sound/wicked/characters/Scavenger/09.ogg',
-                '10': 'sound/wicked/characters/Scavenger/10.ogg',
-                '11': 'sound/wicked/characters/Scavenger/11.ogg',
-                '12': 'sound/wicked/characters/Scavenger/12.ogg',
-            },
-            'wickedq1-syndrome': {
-                '01': 'sound/wicked/characters/Syndrome/01.ogg',
-                '02': 'sound/wicked/characters/Syndrome/02.ogg',
-                '03': 'sound/wicked/characters/Syndrome/03.ogg',
-                '04': 'sound/wicked/characters/Syndrome/04.ogg',
-                '05': 'sound/wicked/characters/Syndrome/05.ogg',
-                '06': 'sound/wicked/characters/Syndrome/06.ogg',
-                '07': 'sound/wicked/characters/Syndrome/07.ogg',
-                '08': 'sound/wicked/characters/Syndrome/08.ogg',
-                '09': 'sound/wicked/characters/Syndrome/09.ogg',
-                '10': 'sound/wicked/characters/Syndrome/10.ogg',
-                '11': 'sound/wicked/characters/Syndrome/11.ogg',
-                '12': 'sound/wicked/characters/Syndrome/12.ogg',
-            },
-            'wickedq1-thelord': {
-                '01': 'sound/wicked/characters/TheLord/01.ogg',
-                '02': 'sound/wicked/characters/TheLord/02.ogg',
-                '03': 'sound/wicked/characters/TheLord/03.ogg',
-                '04': 'sound/wicked/characters/TheLord/04.ogg',
-                '05': 'sound/wicked/characters/TheLord/05.ogg',
-                '06': 'sound/wicked/characters/TheLord/06.ogg',
-                '07': 'sound/wicked/characters/TheLord/07.ogg',
-                '08': 'sound/wicked/characters/TheLord/08.ogg',
-                '09': 'sound/wicked/characters/TheLord/09.ogg',
-                '10': 'sound/wicked/characters/TheLord/10.ogg',
-                '11': 'sound/wicked/characters/TheLord/11.ogg',
-                '12': 'sound/wicked/characters/TheLord/12.ogg',
-            },
-            'wickedq1-wotan': {
-                '01': 'sound/wicked/characters/Wotan/01.ogg',
-                '02': 'sound/wicked/characters/Wotan/02.ogg',
-                '03': 'sound/wicked/characters/Wotan/03.ogg',
-                '04': 'sound/wicked/characters/Wotan/04.ogg',
-                '05': 'sound/wicked/characters/Wotan/05.ogg',
-                '06': 'sound/wicked/characters/Wotan/06.ogg',
-                '07': 'sound/wicked/characters/Wotan/07.ogg',
-                '08': 'sound/wicked/characters/Wotan/08.ogg',
-                '09': 'sound/wicked/characters/Wotan/09.ogg',
-                '10': 'sound/wicked/characters/Wotan/10.ogg',
-                '11': 'sound/wicked/characters/Wotan/11.ogg',
-                '12': 'sound/wicked/characters/Wotan/12.ogg',
-            },
-            'officespace': {
-                'diemothafuckas': 'sound/westcoastcrew/diemothafuckas.ogg',
-            }
-        }
-        return self._mapping
+        self.db[player_key] = datetime.now().timestamp()
+        return True
+
+    def register_shouts(self):
+        """
+        Registers shouts in our local instance of `Shoutdb`
+        """
+        characters = [
+            'Badblood', 'Buzz', 'DarthVader',
+            'DocHoliday', 'Moses', 'Peedee',
+            'TheLord', 'Scavenger', 'Wotan'
+        ]
+        char_shouts = []
+        for character in characters:
+            for index in range(1, 13):
+                char_shouts.append(
+                    Shout(
+                        'wickedq1-%s' % character.lower(),
+                        '%02d' % index,
+                        'characters/%s/%02d.ogg' % (character, index)))
+
+        self.shoutdb.register_shouts(char_shouts)
+        self.shoutdb.register_shouts([
+            Shout('5thelement', 'omg', 'shouts/5th/omg.ogg'),
+            Shout('5thelement', 'pipolei', 'shouts/5th/pipolei.ogg'),
+            Shout('5thelement', 'supergreen', 'shouts/5th/super_green.ogg'),
+
+            Shout('aliens', 'gameover', 'shouts/aliens/game_over.ogg'),
+            Shout('aliens', 'ready', 'shouts/aliens/ready_to_get_it_on.ogg'),
+            Shout('aliens', 'stop', 'shouts/aliens/stop_your_grinnin.ogg'),
+            Shout('aliens', 'badass', 'shouts/aliens/ultimate_badass.ogg'),
+
+            Shout('aod', 'arrogance', 'shouts/aod/arrogance.ogg'),
+            Shout('aod', 'boomstick', 'shouts/aod/boomstick.ogg'),
+            Shout('aod', 'comegetsome', 'shouts/aod/comegetsome.ogg'),
+            Shout('aod', 'cometopappa', 'shouts/aod/cometopappa.ogg'),
+            Shout('aod', 'fancypants', 'shouts/aod/fancypants.ogg'),
+            Shout('aod', 'groovy', 'shouts/aod/groovy.ogg'),
+            Shout('aod', 'guywithgun', 'shouts/aod/guywithgun.ogg'),
+            Shout('aod', 'killkiss', 'shouts/aod/killkiss.ogg'),
+            Shout('aod', 'laughter', 'shouts/aod/laughter.ogg'),
+            Shout('aod', 'liveagain', 'shouts/aod/liveagain.ogg'),
+            Shout('aod', 'miserable', 'shouts/aod/miserable.ogg'),
+            Shout('aod', 'run', 'shouts/aod/run.ogg'),
+            Shout('aod', 'shopsmart', 'shouts/aod/shopsmart.ogg'),
+        ])
